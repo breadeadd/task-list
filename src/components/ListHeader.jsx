7d7948bda73dd13c5e47c1ca43d9c223958e622a
Caitlin Kuan <caitlinkuan49@gmail.com>
2026-03-14 13:26:37 +1300

    Ability to add multiple lists

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const ListHeader = () => {
     const [isEditing, setIsEditing] = useState(false);
-    const [title, setTitle] = useState("Enter List Name");
+    const [title, setTitle] = useState("Enter list name");
 
     const toggleEdit = () => {
         setIsEditing(!isEditing);
@@ -10,28 +10,24 @@ const ListHeader = () => {
 
   return (
     <div className="listHeader">
-        <h3>
-            {isEditing ? (
-                <>
-                    <input
-                        type = "text"
-                        value = {title}
-                        autoFocus
-                        onChange={(e) => setTitle(e.target.value)}
-                        onKeyDown={(e) => {if (e.key === "Enter") toggleEdit()}}
-                    />
-                    <i onClick={toggleEdit} class="fa-regular fa-floppy-disk"></i>
-                </>
-            ) : (
-                <>
-                    {title}
-                    <i onClick = {toggleEdit} className="fa-solid fa-pencil"></i>
-                </>
-            )}
-        </h3>
+        {isEditing ? (
+            <>
+                <input
+                    type = "text"
+                    value = {title}
+                    autoFocus
+                    onChange={(e) => setTitle(e.target.value)}
+                    onKeyDown={(e) => {if (e.key === "Enter") toggleEdit()}}
+                />
+                <i onClick={toggleEdit} class="fa-regular fa-floppy-disk"></i>
+            </>
+        ) : (
+            <>
+                {title}
+                <i onClick = {toggleEdit} className="fa-solid fa-pencil"></i>
+            </>
+        )}
 
-
-        <i class="fa-solid fa-plus"></i>
     </div>
   )
 }
--- src/components/ListsContainer.jsx
+++ src/components/ListsContainer.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ListHeader from './ListHeader'
 import TodoCard from './TodoCard'
 
 const ListsContainer = () => {
+  const [lists, setLists] = useState([
+    {id: 1, title: "Enter list name..."}
+  ])
+
+  //Adding new item
+  const addList = () => {
+    const newList = {
+      id: Date.now(),
+      title: "New List"
+    };
+    setLists([...lists, newList]);
+  };
+
   return (
-    <ListHeader />
+    <div className = "listContainer">
+        <div style={{ marginLeft: 'auto' }}>
+          <i onClick={addList} class="fa-solid fa-plus"></i>
+        </div>
+        {lists.map((list) => (
+          <ListHeader key={list.id} initialTitle={list.title} />
+        ))}
+    </div>
   )
 }
 
